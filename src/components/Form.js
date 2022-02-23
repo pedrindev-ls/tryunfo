dc3a9952c3e5311577dd8c777ec82eeca71d7ba4
Pedrin <pedro515vieira@gmail.com>
2022-02-23 18:44:51 -0300

    Novos acertos no requisito 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -94,7 +94,7 @@ class Form extends Component {
           >
             <option value="normal">normal</option>
             <option value="raro">raro</option>
-            <option value="muito-raro">muito raro</option>
+            <option value="muito raro">muito raro</option>
           </select>
         </label>
         <label htmlFor="trunfo">
